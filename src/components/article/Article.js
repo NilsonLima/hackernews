a2c507c65752e888e3d95086380e85928c9f6198
NilsonDeLima <nlj@cin.ufpe.br>
2018-12-24 01:47:17 -0300

    feat(Article): component to display an article card

---
+++ src/components/article/Article.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+
+import './Article.css';
+
+const Article = (props) => {
+  const {
+    article: {
+      source,
+      title,
+      url,
+      urlToImage,
+      publishedAt,
+      description,
+    },
+  } = props;
+
+  const momentDate = moment(publishedAt, 'YYYY-MM-DD HH:mm:ss');
+
+  return (
+    <a
+      href={url}
+      rel="noopener noreferrer"
+      target="_blank"
+      className="article-wrapper"
+    >
+      <img
+        src={urlToImage}
+        className="article-image"
+        alt={description}
+      />
+      <div className="article-body">
+        <span id="title">
+          {title.substr(0, title.lastIndexOf('-'))}
+        </span>
+        <div id="source">
+          <span>{source.name}</span>
+          <span>&nbsp;· </span>
+          <span>{momentDate.fromNow(true)}</span>
+        </div>
+      </div>
+    </a>
+  );
+};
+
+Article.propTypes = {
+  article: PropTypes.shape({
+    source: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }).isRequired,
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    urlToImage: PropTypes.string.isRequired,
+    publishedAt: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Article;
