0de17e335cb3d5f289f0b894c6c06e1a658e14ea
NilsonDeLima <nlj@cin.ufpe.br>
2018-12-24 01:48:05 -0300

    refactor(App): display headline articles in a grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import NewsAPI from './news';
 import './App.css';
 
+import Article from './components/article';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { articles: [] };
+  }
+
+  async componentDidMount() {
+    const response = await NewsAPI.fetchTopHeadlinesByCategory('general');
+    this.setState({ articles: response.articles });
+  }
+
   render() {
+    const { articles } = this.state;
     return (
-      <div className="App">
+      <div className="app">
+        <div className="app-feed">
+          <div className="app-articles">
+            { articles.map(a => (
+              <Article key={a.url} article={a} />
+            )) }
+          </div>
+        </div>
       </div>
     );
   }
