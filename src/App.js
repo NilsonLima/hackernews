6180736e22d07a115d161202c3b3599e67655b45
NilsonDeLima <nlj@cin.ufpe.br>
2018-12-30 10:57:37 -0300

    feat(App): infinite scroll on selected category articles

--- src/App.js
+++ src/App.js
@@ -2,24 +2,75 @@ import React, { Component } from 'react';
 import NewsAPI from './news';
 import './App.css';
 
+import Constants from './constants';
 import Article from './components/article';
+import SearchBar from './components/searchbar';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { articles: [] };
+    this.state = { category: 'general', page: 1, articles: [] };
+    this.setArticlesByCategory();
+
+    this.setArticlesByCategory = this.setArticlesByCategory.bind(this);
+    this.handleCategoryPress = this.handleCategoryPress.bind(this);
+    this.trackScrolling = this.trackScrolling.bind(this);
   }
 
-  async componentDidMount() {
-    const response = await NewsAPI.fetchTopHeadlinesByCategory('general');
-    this.setState({ articles: response.articles });
+  componentDidMount() {
+    this.firstCategory.focus();
+    document.addEventListener('scroll', this.trackScrolling);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.trackScrolling);
+  }
+
+  setArticlesByCategory() {
+    const { category, page } = this.state;
+    NewsAPI
+      .fetchTopHeadlinesByCategory(category, page)
+      .then(res => this.setState(state => (
+        { articles: [...state.articles, ...res.articles] }
+      )));
+  }
+
+  handleCategoryPress(event, category) {
+    event.preventDefault();
+    this.setState({ category, page: 1, articles: [] }, this.setArticlesByCategory);
+  }
+
+  trackScrolling() {
+    const wrapped = document.getElementById('app-container');
+    if (wrapped.getBoundingClientRect().bottom <= window.innerHeight) {
+      this.setState(state => ({ page: state.page + 1 }), this.setArticlesByCategory);
+    }
   }
 
   render() {
     const { articles } = this.state;
     return (
-      <div className="app">
+      <div id="app-container" className="app-container">
+        <div className="app-header">
+          <SearchBar />
+        </div>
         <div className="app-feed">
+          <div
+            className="app-categories"
+            ref={(ref) => { this.categories = ref; }}
+          >
+            { Constants.Categories.map((c, i) => (
+              <button
+                key={c}
+                type="button"
+                className="app-category"
+                ref={(ref) => { if (!i) this.firstCategory = ref; }}
+                onClick={e => this.handleCategoryPress(e, c)}
+              >
+                { c }
+              </button>
+            )) }
+          </div>
           <div className="app-articles">
             { articles.map(a => (
               <Article key={a.url} article={a} />
